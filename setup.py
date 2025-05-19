9e656d13f09e5d8226d944e666d5b5ffc89b877d
Chirag Jogi <chiragjogi97@gmail.com>
2025-05-20 00:19:55 +0530

    COMMIT

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements=f.read().splitlines()
+
+
+setup(
+    name="House Reservation Prediction",
+    version="0.1",
+    author='Chirag',
+    packages=find_packages(),
+    install_requires=requirements
+
+)
---
+++ src/custom_exception.py
@@ -0,0 +1,26 @@
+import traceback
+import sys
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=self.get_detailed_error_message(error_message,error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message , error_detail=None):
+
+       if error_detail is None:
+        _ , _ , exc_tb= error_detail.exc_info()
+       else:
+        exc_tb=error_detail.__traceback__
+
+       if exc_tb is None:
+        return error_message 
+       
+       file_name=exc_tb.tb_frame.f_code.co_filename
+       line_number=exc_tb.tb_lineno
+
+       return f"Error in {file_name} ,line {line_number} : {error_message}"
+    
+    def __str__(self):
+        return self.error_message
